ff8a0d34e63444cefda70bf72101ab6db94a88ac
millan-figueroa <millan.fig@gmail.com>
2025-02-22 21:33:45 -0800

    Adds styles to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,12 @@
 export default function Header() {
-  return <h1>Meme Generator</h1>;
+  return (
+    <div className="relative flex items-center w-full h-[60px] bg-gradient-to-r from-red-500 to-pink-400 px-4">
+      <img
+        className="w-[40px] h-[40px] mr-2"
+        src="./ghostface.png"
+        alt="troll face"
+      />
+      <h1 className="text-lg font-bold text-white">Meme Generator</h1>
+    </div>
+  );
 }
