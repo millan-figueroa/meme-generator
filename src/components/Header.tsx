05087f76d164bce3fd0e028fa16fce85ff3ad40b
millan-figueroa <millan.fig@gmail.com>
2025-02-22 21:38:17 -0800

    Updates png and change color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 export default function Header() {
   return (
-    <div className="relative flex items-center w-full h-[60px] bg-gradient-to-r from-red-500 to-pink-400 px-4">
+    <div className="relative flex items-center w-full h-[60px] bg-gradient-to-r from-purple-500 to-pink-400 px-4">
       <img
-        className="w-[40px] h-[40px] mr-2"
+        className="ml-3 w-[40px] h-[40px] mr-2"
         src="./ghostface.png"
         alt="troll face"
       />
-      <h1 className="text-lg font-bold text-white">Meme Generator</h1>
+      <h1 className="ml-1 text-lg font-bold text-white">Meme Generator</h1>
     </div>
   );
 }
