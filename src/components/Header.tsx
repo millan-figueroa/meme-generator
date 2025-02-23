360420b739e12434f75d938d034c40a1e9618b13
millan-figueroa <millan.fig@gmail.com>
2025-02-22 18:13:06 -0800

    Adds scaffold code for Main component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,3 @@
 export default function Header() {
-  return <h1>This is Header.</h1>;
+  return <h1>Meme Generator</h1>;
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,33 @@
-export default function Main() {
-  return <h2>This is Main.</h2>;
+import * as React from "react";
+
+export default function Main(): React.JSX.Element {
+  return (
+    <main>
+      <div>
+        <label>
+          Top Text
+          <input
+            type="text"
+            className="border border-gray-300 rounded-md p-2"
+            placeholder="Enter top text"
+          />
+        </label>
+
+        <label>
+          Bottom Text
+          <input
+            type="text"
+            className="border border-gray-300 rounded-md p-2"
+            placeholder="Enter bottom text"
+          />
+        </label>
+        <button>Get a new meme image</button>
+      </div>
+      <div className="meme">
+        <img src="http://i.imgflip.com/1bij.jpg" alt="image" />
+        <span className="top">One does not simply</span>
+        <span className="bottom">Walk into Mordor</span>
+      </div>
+    </main>
+  );
 }
