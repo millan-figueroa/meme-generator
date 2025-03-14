40f293c7d10f230ac017a2162253700329877257
millan-figueroa <millan.fig@gmail.com>
2025-03-14 15:12:13 -0700

    Makes Header component mobile responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,13 @@ export default function Header() {
   return (
     <div className="relative flex items-center w-full h-[60px] bg-gradient-to-r from-purple-500 to-pink-400 px-4">
       <img
-        className="ml-3 w-[40px] h-[40px] mr-2"
+        className="ml-1 w-[30px] h-[30px] sm:w-[40px] sm:h-[40px] mr-3"
         src="./ghostface.png"
         alt="troll face"
       />
-      <h1 className="ml-1 text-lg font-bold text-white">Meme Generator</h1>
+      <h1 className="text-lg sm:text-xl md:text-2xl font-bold text-white">
+        Meme Generator
+      </h1>
     </div>
   );
 }
