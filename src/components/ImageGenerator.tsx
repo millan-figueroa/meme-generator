0ea03cce9dfcf5e2139138f54e9cfe07b9e9cb78
millan-figueroa <millan.fig@gmail.com>
2025-03-14 15:23:23 -0700

    Updates pre-defined w/h of button for mobile responsiveness, adds other styles

--- src/components/ImageGenerator.tsx
+++ src/components/ImageGenerator.tsx
@@ -4,15 +4,15 @@ export default function ImageGenerator() {
   const [randomImage, setRandomImage] = React.useState<string>("");
 
   return (
-    <div className="flex-direction-column justify-items-center">
-      <button className="w-[450px] h-[50px] my-4 bg-gradient-to-r from-purple-500 to-pink-400 border-gray-200 rounded-lg">
-        Get a new meme image
+    <div className="flex-direction-column justify-items-center m-4">
+      <button className="w-full py-2 px-6 my-6 bg-gradient-to-r from-purple-500 to-pink-400 border-gray-200 rounded-lg">
+        Get a new image
       </button>
 
       <img
         src="http://i.imgflip.com/1bij.jpg"
         alt="image"
-        className="rounded-lg"
+        className="rounded-lg border-2 border-white"
       />
     </div>
   );
