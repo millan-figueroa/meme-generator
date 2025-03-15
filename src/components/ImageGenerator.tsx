68d31a41ba55584909dabf1d7ec4fed776e182eb
millan-figueroa <millan.fig@gmail.com>
2025-03-14 17:10:34 -0700

    Adds and styles spans that contain meme text

--- src/components/ImageGenerator.tsx
+++ src/components/ImageGenerator.tsx
@@ -4,16 +4,23 @@ export default function ImageGenerator() {
   const [randomImage, setRandomImage] = React.useState<string>("");
 
   return (
-    <div className="flex-direction-column justify-items-center mx-6">
-      <button className="w-full py-2 px-6 my-6 bg-gradient-to-r from-purple-500 to-pink-400 rounded-lg">
+    <div className="flex flex-col justify-items-center mx-6">
+      <button className="w-full py-2 px-6 mb-6 text-white bg-gradient-to-r from-purple-500 to-pink-400 rounded-lg">
         Get a new image
       </button>
-
-      <img
-        src="http://i.imgflip.com/1bij.jpg"
-        alt="image"
-        className="rounded-lg border-2 border-slate-300"
-      />
+      <div className="relative flex flex-col justify-center items-center">
+        <img
+          src="http://i.imgflip.com/1bij.jpg"
+          alt="image"
+          className="rounded-lg"
+        />
+        <span className="absolute text-center my-[15px] px-[5px] font-anton text-3xl uppercase text-white tracking-[1px] [text-shadow:_2px_2px_0_#000,_-2px_-2px_0_#000,_2px_-2px_0_#000,_-2px_2px_0_#000,_0_2px_0_#000,_2px_0_0_#000,_0_-2px_0_#000,_-2px_0_0_#000,_2px_2px_5px_#000] top-0">
+          Some Text
+        </span>
+        <span className="absolute text-center my-[15px] px-[5px] font-anton text-3xl uppercase text-white tracking-[1px] [text-shadow:_2px_2px_0_#000,_-2px_-2px_0_#000,_2px_-2px_0_#000,_-2px_2px_0_#000,_0_2px_0_#000,_2px_0_0_#000,_0_-2px_0_#000,_-2px_0_0_#000,_2px_2px_5px_#000] bottom-0">
+          Some Text
+        </span>
+      </div>
     </div>
   );
 }
