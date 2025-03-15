eaf293d862fb3186e376be0e0d4b20d138afa9b5
millan-figueroa <millan.fig@gmail.com>
2025-03-14 17:13:21 -0700

    Loads google font into project and updates font in tailwind config file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,28 @@ import Header from "@/components/Header";
 import Inputs from "@/components/Inputs";
 import ImageGenerator from "@/components/ImageGenerator";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="h-screen bg-gray-800">
-      <Header />
-      <Inputs />
-      <ImageGenerator />
-    </div>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Anton&family=Phudu:wght@300..900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="h-screen">
+        <Header />
+        <Inputs />
+        <ImageGenerator />
+      </div>
+    </>
   );
 }
