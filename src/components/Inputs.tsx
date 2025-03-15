32cf1fe90092bca797b93d4e0aa0485c4afe6c9d
millan-figueroa <millan.fig@gmail.com>
2025-03-14 17:36:08 -0700

    Renames meme useState, populates object with props and creates Meme interface.

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,28 +1,35 @@
-import * as React from "react";
+import { useState } from "react";
+
+interface Meme {
+  topText: string;
+  bottomText: string;
+  imageUrl: string;
+}
 
 export default function Inputs(): React.JSX.Element {
-  const [topText, setTopText] = React.useState<string>("");
-  const [bottomText, setBottomText] = React.useState<string>("");
+  const [meme, setMeme] = useState<Meme>({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
 
   return (
     <main className="flex-direction-column p-6">
       <div className="flex justify-center items-center gap-6">
-        <label className="text-gray-800">
+        <label className="text-white">
           Top Text
-          <br />
           <input
             type="text"
-            className="w-full border border-gray-300 rounded-md p-2"
+            className="w-full border border-gray-400 rounded-md p-2"
             placeholder="Enter top text"
           />
         </label>
 
-        <label className="text-gray-800">
+        <label className="text-white">
           Bottom Text
-          <br />
           <input
             type="text"
-            className="w-full border border-gray-300 rounded-md p-2"
+            className="w-full border border-gray-400 rounded-md p-2"
             placeholder="Enter bottom text"
           />
         </label>
